c23037d36a624dfb563a168377834a38d2f47337
bnnzz <ben31_rc@hotmail.com>
2025-07-24 14:58:53 -0600

    se agregaron cambios de hero y about

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -8,7 +8,7 @@ const About = () => {
     <>
       {/* <!-- ===== About Start ===== --> */}
       <section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
-        <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
+        <div className="max-w-c-1235 mx-auto px-4 md:px-8 xl:px-0">
           <div className="flex items-center gap-8 lg:gap-32.5">
             <motion.div
               variants={{
@@ -59,47 +59,54 @@ const About = () => {
               viewport={{ once: true }}
               className="animate_right md:w-1/2"
             >
-              <span className="font-medium uppercase text-black dark:text-white">
-                <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 text-metatitle uppercase text-white ">
-                  New
+              <span className="font-medium text-black uppercase dark:text-white">
+                <span className="bg-meta text-metatitle mr-4 mb-4 inline-flex rounded-full px-4.5 py-1 text-white uppercase">
+                  Plataforma de Apoyo para Estudiantes Foráneos
                 </span>{" "}
-                SaaS Boilerplate for Next.js
+                BUAP
               </span>
-              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                A Complete Solution for
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
-                  SaaS Startup
+              <h2 className="xl:text-hero relative mb-6 text-3xl font-bold text-black dark:text-white">
+                Una solución completa para
+                <span className="before:bg-titlebg dark:before:bg-titlebgdark relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full">
+                  Estudiantes Foráneos
                 </span>
               </h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                ultricies lacus non fermentum ultrices. Fusce consectetur le.
+                Conecta, resuelve dudas, encuentra alojamiento, y accede a
+                información útil para adaptarte a la vida universitaria en la
+                BUAP.
               </p>
 
               <div className="mt-7.5 flex items-center gap-5">
-                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                <div className="border-stroke dark:border-strokedark dark:bg-blacksection flex h-15 w-15 items-center justify-center rounded-[50%] border">
                   <p className="text-metatitle2 font-semibold text-black dark:text-white">
                     01
                   </p>
                 </div>
                 <div className="w-3/4">
-                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
-                    React 18, Next.js 13 and TypeScript
+                  <h3 className="text-metatitle2 mb-0.5 text-black dark:text-white">
+                    Apoyo real para tu día a día
                   </h3>
-                  <p>Ut ultricies lacus non fermentum ultrices.</p>
+                  <p>
+                    Consejos útiles, comunidad activa y recursos que sí
+                    importan.
+                  </p>
                 </div>
               </div>
               <div className="mt-7.5 flex items-center gap-5">
-                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                <div className="border-stroke dark:border-strokedark dark:bg-blacksection flex h-15 w-15 items-center justify-center rounded-[50%] border">
                   <p className="text-metatitle2 font-semibold text-black dark:text-white">
                     02
                   </p>
                 </div>
                 <div className="w-3/4">
-                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
-                    Fully Customizable
+                  <h3 className="text-metatitle2 mb-0.5 text-black dark:text-white">
+                    Plataforma personalizable
                   </h3>
-                  <p>consectetur adipiscing elit fermentum ultricies.</p>
+                  <p>
+                    Tu experiencia mejora con base en lo que necesitas y
+                    compartes.
+                  </p>
                 </div>
               </div>
             </motion.div>
@@ -110,7 +117,7 @@ const About = () => {
 
       {/* <!-- ===== About Two Start ===== --> */}
       <section>
-        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
+        <div className="max-w-c-1235 mx-auto overflow-hidden px-4 md:px-8 2xl:px-0">
           <div className="flex items-center gap-8 lg:gap-32.5">
             <motion.div
               variants={{
@@ -130,26 +137,27 @@ const About = () => {
               viewport={{ once: true }}
               className="animate_left md:w-1/2"
             >
-              <h4 className="font-medium uppercase text-black dark:text-white">
-                Launch Your SaaS Fast
+              <h4 className="font-medium text-black uppercase dark:text-white">
+                Una nueva etapa en la BUAP
               </h4>
-              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                Packed with All Essential {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
-                  Integrations
+              <h2 className="xl:text-hero relative mb-6 text-3xl font-bold text-black dark:text-white">
+                Con todas las {"   "}
+                <span className="before:bg-titlebg2 dark:before:bg-titlebgdark relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full">
+                  herramientas esenciales
                 </span>
               </h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                ultricies lacus non fermentum ultrices. Fusce consectetur le.
+                Desde alojamiento y comida hasta rutas de transporte y grupos
+                estudiantiles, te damos lo necesario para que te sientas en
+                casa.
               </p>
               <div>
                 <a
                   href="#"
-                  className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary"
+                  className="group hover:text-primary dark:hover:text-primary mt-7.5 inline-flex items-center gap-2.5 text-black dark:text-white"
                 >
                   <span className="duration-300 group-hover:pr-2">
-                    Know More
+                    Explorar más
                   </span>
                   <svg
                     width="14"
