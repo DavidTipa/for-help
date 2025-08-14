5058883ee99dac83c5a601bfad28291ccc528ad2
nabor14 <nabor.andrea.14@gmail.com>
2025-08-13 20:44:18 -0600

    Mejora de guias

--- app/(site)/guias/page.tsx
+++ app/(site)/guias/page.tsx
@@ -1,48 +1,23 @@
 export default function UniversityGuide() {
   const guides = [
     {
-      title: "Inscripción de materias",
-      description: "Inscribe tus materias de forma correcta y sin complicaciones.",
-      image: "/images/guias/materias.png"
-    },
-    {
-      title: "Reposición de Credencial",
-      description: "Solicita una nueva credencial de estudiante en caso de pérdida o daño.",
-      image: "/images/guias/credencial.png"
-    },
-    {
-      title: "Como descargar tu Krdex simple",
-      description: "Como descargar y imprimir tu Kardex simple",
+      title: "Guías Académicas",
+      description: "Aquí encontrarás guías claras para realizar tramites escenciales y simples, todo explicado paso a paso.",
       image: "/images/guias/kardex.jpg"
     },
     {
-      title: "Como descargar tu Constancia de Estudios",
-      description: "Como descargar y imprimir tu Constancia",
-      image: "/images/guias/kardex.jpg"
+      title: "Guías de Vida Universitaria ",
+      description: "Aquí encontrarás guías con recursos para adaptarte a la BUAP",
+      image: "/images/guias/adaptacion.jpg"
     },
     {
-      title: "Cómo usar el Lobo Bus",
-      description: "Guía para utilizar el transporte universitario de manera eficiente.",
-      image: "/images/guias/lobobus.jpg"
-    },
-    {
-      title: "Cómo usar la Lobo Bici",
-      description: "Utiliza de forma correcta las bicicletas de la universidad.",
-      image: "/images/guias/Lobobici.png"
-    },
-    {
-      title: "Becas BUAP",
-      description: "Enterate y registarte de las becas en BUAP.",
+      title: "Guías de servicios y Apoyos",
+      description: "Aquí encontrarás guías sobre becas,orientación psicológica y otros servicios que la universidad ofrece.",
       image: "/images/guias/becas.png"
     },
     {
-      title: "Identificar tu salón rápidamente",
-      description: "Trucos y consejos para encontrar tu salón sin perderte.",
-      image: "/images/guias/salon.png"
-    },
-        {
-      title: "Convocatorias y eventos",
-      description: "Entérate de las últimas convocatorias y actividades BUAP.",
+      title: "Eventos",
+      description: "Aquí encontrarás los eventos mas importantes y actualizaciones",
       image: "/images/guias/eventos.jpg"
     }
   ];
